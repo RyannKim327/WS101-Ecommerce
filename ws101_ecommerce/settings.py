6174327028895f84ff1c8412e7b5585598f5dc47
RyannKim327 <rksesgundo123@gmail.com>
2024-11-21 13:29:41 +0800

    11-21-24 13:29

--- ws101_ecommerce/settings.py
+++ ws101_ecommerce/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ('static', '')
+STATIC_ROOT = ''
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
